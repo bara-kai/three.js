2d1d0b43e7b98317a55fceb7ec6dac2d2350800b
bara-kai <92484983+bara-kai@users.noreply.github.com>
2022-07-16 12:03:15 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  // devtool: 'source-map',
+  entry: './src/javascripts/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'javascripts/[name]-[contenthash].js',
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '.dist'),
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: '> 0.25%, not dead' }],
+                '@babel/preset-react',
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: false },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|JPG)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name]-[contenthash][ext]',
+        },
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './stylesheets/[name]-[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/sample.pug',
+      filename: 'sample.html',
+    }),
+
+    new CleanWebpackPlugin(),
+  ],
+};
